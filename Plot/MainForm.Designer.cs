36e782b3eaf0dc84248c6559fe652049ee21ca4f
ljx <ljxstc@hotmail.com>
2020-11-02 15:09:56 +0800

    添加项目文件。

---
+++ Plot/MainForm.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace Plot
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.plotView1 = new OxyPlot.WindowsForms.PlotView();
+            this.plotView2 = new OxyPlot.WindowsForms.PlotView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // plotView1
+            // 
+            this.plotView1.Location = new System.Drawing.Point(371, 66);
+            this.plotView1.Name = "plotView1";
+            this.plotView1.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView1.Size = new System.Drawing.Size(366, 319);
+            this.plotView1.TabIndex = 0;
+            this.plotView1.Text = "plotView1";
+            this.plotView1.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView1.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView1.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.plotView1.Click += new System.EventHandler(this.plotView1_Click);
+            // 
+            // plotView2
+            // 
+            this.plotView2.Location = new System.Drawing.Point(24, 66);
+            this.plotView2.Name = "plotView2";
+            this.plotView2.PanCursor = System.Windows.Forms.Cursors.Hand;
+            this.plotView2.Size = new System.Drawing.Size(380, 305);
+            this.plotView2.TabIndex = 1;
+            this.plotView2.Text = "plotView2";
+            this.plotView2.ZoomHorizontalCursor = System.Windows.Forms.Cursors.SizeWE;
+            this.plotView2.ZoomRectangleCursor = System.Windows.Forms.Cursors.SizeNWSE;
+            this.plotView2.ZoomVerticalCursor = System.Windows.Forms.Cursors.SizeNS;
+            this.plotView2.MouseDown += new System.Windows.Forms.MouseEventHandler(this.plotView2_MouseDown);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(328, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.plotView2);
+            this.Controls.Add(this.plotView1);
+            this.Name = "MainForm";
+            this.Text = "主界面";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private OxyPlot.WindowsForms.PlotView plotView1;
+        private OxyPlot.WindowsForms.PlotView plotView2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Plot/MainForm.cs
@@ -0,0 +1,55 @@
+﻿using MathNet.Numerics.Data.Matlab;
+using MathNet.Numerics.LinearAlgebra;
+using OxyPlot;
+using OxyPlot.Series;
+using System;
+using System.Windows.Forms;
+namespace Plot
+{
+    public partial class MainForm : Form
+    {
+        /// <summary>
+        /// 鼠标按下为true
+        /// </summary>
+        private bool mouseDown;
+
+        /// <summary>
+        /// 鼠标在事件源的位置
+        /// </summary>
+        private int CurX = 0;
+        private int CurY = 0;
+        public MainForm()
+        {
+            InitializeComponent();
+            var myModel = new PlotModel { Title = "Example 1" };
+            myModel.Series.Add(new FunctionSeries(Math.Cos, 0, 10, 0.1, "cos(x)"));
+            this.plotView1.Model = myModel;
+
+            var myModel2 = new PlotModel { Title = "Example 2" };
+            myModel2.Series.Add(new FunctionSeries(Math.Sin, 0, 10, 0.1, "sin(x)"));
+            this.plotView2.Model = myModel2;
+        }
+
+        /// <summary>
+        /// 鼠标点击plotView2时的事件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void plotView1_Click(object sender, EventArgs e)
+        {
+            
+
+        }
+        
+
+        private void plotView2_MouseDown(object sender, MouseEventArgs e)
+        {
+            CurX = e.X;
+            CurY = e.Y;
+            mouseDown = true;
+           
+        }
+
+      
+    }
+}
